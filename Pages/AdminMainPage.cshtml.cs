7cfe2dd261817abc25364e92e4837728a67432f8
James-T-S <James.Shipway@student.shu.ac.uk>
2025-10-07 11:12:03 +0100

    Add project files.

---
+++ Pages/AdminMainPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace groupwork.Pages
+{
+    public class AdminMainPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/DonatationManagerIncomingDonation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace groupwork.Pages
+{
+    public class DonatationManagerIncomingDonationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
